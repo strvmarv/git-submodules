1a5b495e6bb04b7f3a1b7a19a501f3c2b53d2e18
Paul Marvin <paul.marvin@radancy.com>
2023-09-14 10:41:06 -0500

    Add app content and submodule commit reference

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+using ExampleModule;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/weatherforecast", (WeatherForecastService service) => service.GetWeatherForecast())
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+
